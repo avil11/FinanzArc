5a700effc1b439b95a7f0ae6a03d952a8aa6c61c
Ramiro Avila <ramirovalentin.avila@gmail.com>
2026-04-16 01:01:34 -0300

    General con consumo de apis Usuario, Gasto, Ingreso y MetaAhorro

---
+++ src/componentes/Apis.js
@@ -0,0 +1,11 @@
+// Traemos las apis que vamos a consumir
+export const API_BASE_URL = "http://localhost:60496/api";
+
+export const API_ENDPOINTS = {
+  login: "/auth/login",
+  register: "/auth/register",
+  gastos: "/Gastos",
+  ingresos: "/Ingresos",
+  ahorros: "/Ahorros",
+  perfil: "/Perfil"
+};
